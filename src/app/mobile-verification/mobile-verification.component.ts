bd1ad3a3cb5c73f6e9af3ecc6c7e226b815f8df7
Bithika-Redapple <bithika@redappletech.com>
2020-10-14 19:40:50 +0530

    DTUW-113  REDME file modified

--- src/app/mobile-verification/mobile-verification.component.ts
+++ src/app/mobile-verification/mobile-verification.component.ts
@@ -13,114 +13,114 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import { environment } from '../../environments/environment';
 
 @Component({
-  selector: 'app-mobile-verification',
-  templateUrl: './mobile-verification.component.html',
-  styleUrls: ['./mobile-verification.component.css']
+selector: 'app-mobile-verification',
+templateUrl: './mobile-verification.component.html',
+styleUrls: ['./mobile-verification.component.css']
 })
 export class MobileVerificationComponent implements OnInit {
-  state:any;
-  mobileVerificationForm: FormGroup;  
-  isSubmitted:boolean=false;
-  phoneNumberStorage: any;
-  emailStorage:any;
-  firstWordEmail:any;
-  lastWordEmail:any;
-   
-
-
-  constructor(public authService: AuthService,
-  private fb: FormBuilder,
-  private snackbar: MatSnackBar,
-  private router: Router) {
-       this.mobileVerificationForm = this.fb.group({
-                   verify_mobile: new FormControl('',[Validators.required])                   
-                  
-                    });
-   }
-
-  ngOnInit() {
-
-
-     //user_phone
-     this.state = window.history.state.password; 
-     console.log("this.hero"+this.state);
-     this.phoneNumberStorage=
-     localStorage.getItem('user_phone').substr(localStorage.getItem('user_phone').length - 4);
-     
-     //this.firstWordEmail=localStorage.getItem('email').charAt(0);
-
-    // this.lastWordEmail=localStorage.getItem('email').replace(/@.*/,"");
-
-     //this.lastWordEmail=this.lastWordEmail.substr(//this.lastWordEmail.length - 1);
-     //this.emailStorage=localStorage.getItem('user_email');
-  }
-
-  onNext(event) { 
-     console.log("ok"+event);
-  }
-
-  /*
- * This function is used to save user verification preferences data
-  
- */
-
-   mobileVerifyFormSubmit(){
-      this.isSubmitted = true;         
-      
-      
-      if(this.mobileVerificationForm.valid)
-      {
-
-         let saveData = this.mobileVerificationForm.value; 
-          console.log("saveData"+JSON.stringify(saveData));
-
-               this.router.navigate(['otp-verify'], { 
-                state: { password: this.state } 
-              }); 
-          
-           //this will enable after live api url created. 
-           console.log("environment.phone_code"+environment.phone_code)            
-           this.authService.sendOTP(environment.phone_code+localStorage.getItem('user_phone'))
-            .pipe(first())
-            .subscribe(
-                verifyresponse => {
-                
-                if(verifyresponse.status.status_code == 200)
-                    {
-                        this.snackbar.open(verifyresponse.status.message,'OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-
-                      this.router.navigate(['otp-verify'], { 
-                      state: { password: this.state } 
-                       });
-                      
-                    }
-                   else{
-                     this.snackbar.open(verifyresponse.status.message,'OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-                   }
-                },
-                error => {
-                    this.snackbar.open(error,'OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-                }); 
+state:any;
+mobileVerificationForm: FormGroup;  
+isSubmitted:boolean=false;
+phoneNumberStorage: any;
+emailStorage:any;
+firstWordEmail:any;
+lastWordEmail:any;
 
 
 
-      }
+constructor(public authService: AuthService,
+private fb: FormBuilder,
+private snackbar: MatSnackBar,
+private router: Router) {
+this.mobileVerificationForm = this.fb.group({
+verify_mobile: new FormControl('',[Validators.required])                   
 
-      }
+});
+}
+
+ngOnInit() {
+
+
+//user_phone
+this.state = window.history.state.password; 
+console.log("this.hero"+this.state);
+this.phoneNumberStorage=
+localStorage.getItem('user_phone').substr(localStorage.getItem('user_phone').length - 4);
+
+//this.firstWordEmail=localStorage.getItem('email').charAt(0);
+
+// this.lastWordEmail=localStorage.getItem('email').replace(/@.*/,"");
+
+//this.lastWordEmail=this.lastWordEmail.substr(//this.lastWordEmail.length - 1);
+//this.emailStorage=localStorage.getItem('user_email');
+}
+
+onNext(event) { 
+console.log("ok"+event);
+}
+
+/*
+* This function is used to save user verification preferences data
+
+*/
+
+mobileVerifyFormSubmit(){
+this.isSubmitted = true;         
+
+
+if(this.mobileVerificationForm.valid)
+{
+
+let saveData = this.mobileVerificationForm.value; 
+console.log("saveData"+JSON.stringify(saveData));
+
+this.router.navigate(['otp-verify'], { 
+state: { password: this.state } 
+}); 
+
+//this will enable after live api url created. 
+console.log("environment.phone_code"+environment.phone_code)            
+this.authService.sendOTP(environment.phone_code+localStorage.getItem('user_phone'))
+.pipe(first())
+.subscribe(
+verifyresponse => {
+
+if(verifyresponse.status.status_code == 200)
+{
+this.snackbar.open(verifyresponse.status.message,'OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+
+this.router.navigate(['otp-verify'], { 
+state: { password: this.state } 
+});
+
+}
+else{
+this.snackbar.open(verifyresponse.status.message,'OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+}
+},
+error => {
+this.snackbar.open(error,'OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+}); 
+
+
+
+}
+
+}
 
 }
--- src/app/otp-verification/otp-verification.component.ts
+++ src/app/otp-verification/otp-verification.component.ts
@@ -14,146 +14,146 @@ import { environment } from '../../environments/environment';
 
 
 @Component({
-  selector: 'app-otp-verification',
-  templateUrl: './otp-verification.component.html',
-  styleUrls: ['./otp-verification.component.css']
+selector: 'app-otp-verification',
+templateUrl: './otp-verification.component.html',
+styleUrls: ['./otp-verification.component.css']
 })
 export class OtpVerificationComponent implements OnInit {
-  state:any;
-  localsendOtp:any;
-  otpVerificationForm: FormGroup;  
-  isSubmitted:boolean=false;
-  phoneNumberStorage:any;
-  constructor(
-  public authService: AuthService,
-  private fb: FormBuilder,
-  private snackbar: MatSnackBar,
-  private router: Router) { 
-      this.otpVerificationForm = this.fb.group({
-                   verify_code: new FormControl('',[Validators.required])                   
-                  
-                    });
-  }
+state:any;
+localsendOtp:any;
+otpVerificationForm: FormGroup;  
+isSubmitted:boolean=false;
+phoneNumberStorage:any;
+constructor(
+public authService: AuthService,
+private fb: FormBuilder,
+private snackbar: MatSnackBar,
+private router: Router) { 
+this.otpVerificationForm = this.fb.group({
+verify_code: new FormControl('',[Validators.required])                   
 
-  ngOnInit() {
-     this.state = window.history.state.password; 
-     console.log("this.hero"+this.state);
-     this.localsendOtp=localStorage.getItem('sent_otp');
-    this.phoneNumberStorage=
-     localStorage.getItem('user_phone').substr(localStorage.getItem('user_phone').length - 4);
-  }
-  
-    /*
-      * This function is used for compare user otp verification code
- 
-    */
-    otpVerifyFormSubmit(){
-      this.isSubmitted = true;         
-      
-      
-      if(this.otpVerificationForm.valid)
-      {
+});
+}
 
-         let saveData = this.otpVerificationForm.value; 
-          console.log("saveData"+JSON.stringify(saveData));
-          
+ngOnInit() {
+this.state = window.history.state.password; 
+console.log("this.hero"+this.state);
+this.localsendOtp=localStorage.getItem('sent_otp');
+this.phoneNumberStorage=
+localStorage.getItem('user_phone').substr(localStorage.getItem('user_phone').length - 4);
+}
 
-          
-           //call verify otp api and match
-            this.authService.verifyOtp(environment.phone_code+localStorage.getItem('user_phone'), saveData.verify_code)
-            .pipe(first())
-            .subscribe(
-                verifyresponse => {
+/*
+* This function is used for compare user otp verification code
 
-          
-           console.log("this.verifyresponse"+JSON.stringify(verifyresponse));
+*/
+otpVerifyFormSubmit(){
+this.isSubmitted = true;         
 
-              if(verifyresponse.status.status_code == 200)
-                    {
 
-          
-            //call signin api
+if(this.otpVerificationForm.valid)
+{
 
-            this.authService.login(localStorage.getItem('user_phone'), this.state, true,'phone')
-            .pipe(first())
-            .subscribe(
-                loginresponse => {
-                console.log("###");
-                console.log(loginresponse);
-                if(loginresponse.status.status_code == 200)
-                    {
-                        this.snackbar.open(loginresponse.status.status_message,'OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-                      location.href = '/tax-prepare-profile';
-                    }
-                   else{
-                     this.snackbar.open(loginresponse.status.message,'OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-                   }
-                },
-                error => {
-                    this.snackbar.open(error,'OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-                }); 
-
-                }
-                else{
-                  //verify code not match
-                  this.snackbar.open('Verification code does not match','OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-                }
-
-                 
-                 
-
-                 
-                ////verify otp call
-
-                },
-                error => {
-                    this.snackbar.open(error,'OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-                }); 
-
-             
-          }
-          else
-   {
-     this.snackbar.open('Some fields are mandatory','OK',{
-                        verticalPosition: 'top',
-                        horizontalPosition:'right',
-                        panelClass: ['red-snackbar'],
-                        duration:2000
-                      });
-   }
-          
+let saveData = this.otpVerificationForm.value; 
+console.log("saveData"+JSON.stringify(saveData));
 
 
 
-      }
+//call verify otp api and match
+this.authService.verifyOtp(environment.phone_code+localStorage.getItem('user_phone'), saveData.verify_code)
+.pipe(first())
+.subscribe(
+verifyresponse => {
 
-      
 
-   
+console.log("this.verifyresponse"+JSON.stringify(verifyresponse));
+
+if(verifyresponse.status.status_code == 200)
+{
+
+
+//call signin api
+
+this.authService.login(localStorage.getItem('user_phone'), this.state, true,'phone')
+.pipe(first())
+.subscribe(
+loginresponse => {
+console.log("###");
+console.log(loginresponse);
+if(loginresponse.status.status_code == 200)
+{
+this.snackbar.open(loginresponse.status.status_message,'OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+location.href = '/tax-prepare-profile';
+}
+else{
+this.snackbar.open(loginresponse.status.message,'OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+}
+},
+error => {
+this.snackbar.open(error,'OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+}); 
+
+}
+else{
+//verify code not match
+this.snackbar.open('Verification code does not match','OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+}
+
+
+
+
+
+////verify otp call
+
+},
+error => {
+this.snackbar.open(error,'OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+}); 
+
+
+}
+else
+{
+this.snackbar.open('Some fields are mandatory','OK',{
+verticalPosition: 'top',
+horizontalPosition:'right',
+panelClass: ['red-snackbar'],
+duration:2000
+});
+}
+
+
+
+
+}
+
+
+
+
 
 }
